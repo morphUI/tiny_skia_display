fec0a5e8caeb320b656673f8c41b69d6930664a0
Florian Blasius <flovanpt@posteo.de>
2021-03-11 09:53:47 +0100

    work on drawing string

--- src/font/mod.rs
+++ src/font/mod.rs
@@ -3,6 +3,7 @@ use std::marker::PhantomData;
 use embedded_graphics_core::{
     draw_target::DrawTarget,
     prelude::*,
+    primitives::Rectangle,
     text::{CharacterStyle, DecorationColor, TextMetrics, TextRenderer, VerticalAlignment},
 };
 use tiny_skia::{FillRule, Paint, PathBuilder, Pixmap, Transform};
@@ -37,6 +38,71 @@ impl<C: PixelColor> FontTextStyle<C> {
             .text_color(text_color)
             .font_size(font_size)
             .build()
+    }
+
+    /// Resolves a decoration color.
+    fn resolve_decoration_color(&self, color: DecorationColor<C>) -> Option<C> {
+        match color {
+            DecorationColor::None => None,
+            DecorationColor::TextColor => self.text_color,
+            DecorationColor::Custom(c) => Some(c),
+        }
+    }
+
+    fn draw_background<D>(
+        &self,
+        width: u32,
+        position: Point,
+        target: &mut D,
+    ) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = C>,
+    {
+        if width == 0 {
+            return Ok(());
+        }
+
+        if let Some(background_color) = self.background_color {
+            target.fill_solid(
+                &Rectangle::new(position, Size::new(width, self.font_size)),
+                background_color,
+            )?;
+        }
+
+        Ok(())
+    }
+
+    fn draw_strikethrough<D>(
+        &self,
+        width: u32,
+        position: Point,
+        target: &mut D,
+    ) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = C>,
+    {
+        if let Some(strikethrough_color) = self.resolve_decoration_color(self.strikethrough_color) {
+            let top_left = position + Point::new(0, 0);
+            let size = Size::new(width, self.font_size);
+
+            target.fill_solid(&Rectangle::new(top_left, size), strikethrough_color)?;
+        }
+
+        Ok(())
+    }
+
+    fn draw_underline<D>(&self, width: u32, position: Point, target: &mut D) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = C>,
+    {
+        if let Some(underline_color) = self.resolve_decoration_color(self.underline_color) {
+            let top_left = position + Point::new(0, 0);
+            let size = Size::new(width, self.font_size);
+
+            target.fill_solid(&Rectangle::new(top_left, size), underline_color)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -226,7 +292,45 @@ impl<C: PixelColor> TextRenderer for FontTextStyle<C> {
     where
         D: DrawTarget<Color = Self::Color>,
     {
-        todo!()
+        let mut p = position;
+
+        let scale = rusttype::Scale::uniform(self.font_size as f32);
+
+        let v_metrics = self.font.inner.v_metrics(scale);
+        let offset = rusttype::point(0.0, v_metrics.ascent);
+
+        let glyphs: Vec<rusttype::PositionedGlyph> =
+            self.font.inner.layout(text, scale, offset).collect();
+
+        let mut glyph_tracer = GlyphTracer {
+            path_builder: PathBuilder::new(),
+            position: rusttype::point(0.0, 0.0),
+        };
+
+        let mut width = 0;
+
+        for g in glyphs.iter() {
+            let mut gpos = match g.pixel_bounding_box() {
+                Some(bbox) => rusttype::point(bbox.min.x as f32, bbox.min.y as f32),
+                None => {
+                    continue;
+                }
+            };
+            gpos.x += position.x as f32;
+            gpos.y += position.y as f32;
+            glyph_tracer.position = gpos;
+            g.build_outline(&mut glyph_tracer);
+
+            p = Point::new(gpos.x as i32, p.y);
+            width +=
+                (g.position().x as f32 + g.unpositioned().h_metrics().advance_width).ceil() as u32;
+        }
+
+        self.draw_background(width, position, target)?;
+        self.draw_strikethrough(width, position, target)?;
+        self.draw_underline(width, position, target)?;
+
+        Ok(p)
     }
 
     fn draw_whitespace<D>(
@@ -242,28 +346,31 @@ impl<C: PixelColor> TextRenderer for FontTextStyle<C> {
     }
 
     fn measure_string(&self, text: &str, position: Point) -> TextMetrics {
-        // let scale = rusttype::Scale::uniform(size as f32);
-        // let v_metrics = self.inner.v_metrics(scale);
-        // let offset = rusttype::point(0.0, v_metrics.ascent);
+        let scale = rusttype::Scale::uniform(self.font_size as f32);
+        let v_metrics = self.font.inner.v_metrics(scale);
+        let offset = rusttype::point(0.0, v_metrics.ascent);
 
-        // let pixel_height = size.ceil();
+        let glyphs: Vec<rusttype::PositionedGlyph> =
+            self.font.inner.layout(text, scale, offset).collect();
 
-        // // Glyphs to draw for "RustType". Feel free to try other strings.
-        // let glyphs: Vec<rusttype::PositionedGlyph> =
-        //     self.inner.layout(text, scale, offset).collect();
+        let width = glyphs
+            .iter()
+            .rev()
+            .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
+            .next()
+            .unwrap_or(0.0)
+            .ceil() as f64;
 
-        // let width = glyphs
-        //     .iter()
-        //     .rev()
-        //     .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
-        //     .next()
-        //     .unwrap_or(0.0)
-        //     .ceil() as f64;
-        todo!()
+        let size = Size::new(width as u32, self.font_size);
+
+        TextMetrics {
+            bounding_box: Rectangle::new(position, size),
+            next_position: position + size.x_axis(),
+        }
     }
 
-    fn vertical_offset(&self, position: Point, vertical_alignment: VerticalAlignment) -> Point {
-        todo!()
+    fn vertical_offset(&self, position: Point, _vertical_alignment: VerticalAlignment) -> Point {
+        position
     }
 
     fn line_height(&self) -> u32 {
